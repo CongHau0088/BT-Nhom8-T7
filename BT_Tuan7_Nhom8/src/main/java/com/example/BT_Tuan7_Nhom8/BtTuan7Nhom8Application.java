0b92fcbbbf95ffafcbef7755578ba2381a081d50
CongHau0088 <tduy7545@gmail.com>
2024-06-12 19:59:02 +0700

    #Inital Commit

---
+++ BT_Tuan7_Nhom8/src/main/java/com/example/BT_Tuan7_Nhom8/BtTuan7Nhom8Application.java
@@ -0,0 +1,13 @@
+package com.example.BT_Tuan7_Nhom8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtTuan7Nhom8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtTuan7Nhom8Application.class, args);
+	}
+
+}
---
+++ BT_Tuan7_Nhom8/src/test/java/com/example/BT_Tuan7_Nhom8/BtTuan7Nhom8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BT_Tuan7_Nhom8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtTuan7Nhom8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
